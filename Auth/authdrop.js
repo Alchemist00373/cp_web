4f9fc473497c83bea997c6ba384aeacef95a5365
Simon Mariano <simonjay2345@gmail.com>
2025-10-16 21:20:38 +0800

    corrected signup js

--- Auth/authdrop.js
+++ Auth/authdrop.js
@@ -37,6 +37,7 @@ onAuthStateChanged(auth, async (user) => {
   if (user) {
     // Logged in
     authButtons.style.display = "none";
+    authButtons.hidden = true;
     profileDropdown.hidden = false;
 
     try {
